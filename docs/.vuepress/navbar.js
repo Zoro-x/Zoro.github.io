ad87d24992af89f87cbda846616b321993ee5f7b
596777598@qq.com <596777598@qq.com>
2024-07-02 23:44:39 +0800

    feat:调整导航栏

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -1,14 +1,6 @@
 export default [ // 导航栏配置
     { text: '首页', link: '/' },
-    {
-        text: '前端基础',
-        children: [
-            { text: 'HTML', link: '/article/html/' },
-            { text: 'Css', link: '/article/css/' },
-            { text: 'JavaScript', link: '/article/javascript/' },
-            { text: 'TypeScript', link: '/article/typescript/' },
-        ]
-    },
+
     {
         text: '计算机基础',
         ariaLabel: '计算机基础',
@@ -19,6 +11,14 @@ export default [ // 导航栏配置
             { text: '操作系统', link: '/article/操作系统/' },
             { text: '设计模式', link: '/article/设计模式/' },
             { text: 'Web安全', link: '/article/Web安全/' }
+        ]
+    },
+    {
+        text: '前端基础',
+        children: [
+            { text: 'Css', link: '/article/css/CSS基础' },
+            { text: 'JavaScript', link: '/article/javascript/' },
+            { text: 'TypeScript', link: '/article/typescript/' },
         ]
     },
     {
@@ -49,6 +49,8 @@ export default [ // 导航栏配置
         ariaLabel: 'node.js',
         children: [
             { text: 'Node.js', link: '/article/node/' },
+            { text: 'Node 部署', link: '/article/node/使用docker服务node部署' },
+            { text: 'Node 进程管理', link: '/article/node/Node进程管理' },
             { text: 'Linux', link: '/article/linux/' },
             { text: 'MySQL', link: '/article/mysql/' },
             { text: 'Nginx', link: '/article/nginx/' },
@@ -112,13 +114,17 @@ export default [ // 导航栏配置
             { text: '微前端技术方案', link: '/article/微前端/微前端技术方案.md' },
             { text: '微前端框架', link: '/article/微前端/微前端框架.md' },
             { text: '使用微前端重构项目', link: '/article/微前端/使用微前端重构项目.md' },
+            { text: '从零到一搭建微前端项目模板', link: '/article/微前端/从零到一搭建微前端项目模板.md' },
         ]
     },
     {
         text: '低代码平台',
         ariaLabel: '低代码平台',
         children: [
-            { text: '低代码平台', link: '/article/低代码/搭建低代码平台.md' }
+            { text: '低代码平台', link: '/article/低代码/可视化低代码原理解析.md' },
+            { text: '低代码平台', link: '/article/低代码/可视化低代码服务端.md' },
+            { text: '低代码平台', link: '/article/低代码/搭建低代码平台.md' },
+
         ]
     },
 ]
